620d76768713337c4b4d0c9701d2fd9d04efe48f
HarounaTraore <167339173+HarounaTraore@users.noreply.github.com>
2024-09-13 11:56:04 +0000

    Project Initialization

---
+++ src/db/db.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "harouna",
+  database: "order_management",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const connection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Connected using connection pool!");
+    return connection;
+  } catch (err) {
+    console.error("Connection failed: ", err.message);
+    throw err;
+  }
+};
+
+module.exports = { pool, connection };
