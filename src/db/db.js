e0d183c9bf3759103c2a8aa63cfdb9e6f5940148
AbderahmaneThimbo <abderahmanethimbo@gmail.com>
2024-09-23 20:20:43 +0000

    Added Models

--- src/db/db.js
+++ src/db/db.js
@@ -1,25 +1,26 @@
 import { config } from "dotenv";
 import { createPool } from "mysql2/promise";
+
 config();
 
 export const pool = createPool({
-  host: process.env.HOST,
-  user: process.env.USE,
-  port: process.env.PORT,
-  password: process.env.PASSWORD,
-  database: process.env.dbNAME,
-  waitForConnections: process.env.WFC,
-  connectionLimit: parseInt(process.env.CL),
-  queueLimit: parseInt(process.env.QL),
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: process.env.WFC === 'true',  
+  connectionLimit: parseInt(process.env.CL) || 10, 
+  queueLimit: parseInt(process.env.QL) || 0 
 });
 
 const connection = async () => {
   try {
-    const connection = await pool.getConnection();
+    const conn = await pool.getConnection();
     console.log("Connected using connection pool!");
-    return connection;
+    return conn;
   } catch (err) {
     console.error("Connection failed: ", err.message);
-    throw err;
   }
 };
+connection();
