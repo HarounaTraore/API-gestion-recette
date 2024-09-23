aeb1ef71296271c019bc5a516cca2b9483a3c485
HarounaTraore <167339173+HarounaTraore@users.noreply.github.com>
2024-09-23 20:42:25 +0000

    Update Validator

--- src/Validator.js
+++ src/Validator.js
@@ -30,6 +30,55 @@ const addRequestValidator = [
   },
 ];
 
+const updateRequestValidator = [
+  param("id")
+    .notEmpty()
+    .withMessage("Id est requis!")
+    .bail()
+    .custom(async (value, { req }) => {
+      const result = await Recette.getElement(value);
+      if (result === 0) {
+        throw new Error("Cette recette n'existe pas!");
+      }
+      return true;
+    }),
+  check("titre")
+    .notEmpty()
+    .withMessage("Titre ne doit pas être vide")
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage("Minimum 6 caractères requis!")
+    .bail()
+    .custom(async (value, { req }) => {
+      const result = await Recette.checkRecette(value);
+      if (result !== 0) {
+        throw new Error("Cette recette existe déjà!");
+      }
+      return true;
+    }),
+  check("ingrédients")
+    .notEmpty()
+    .withMessage("Ingredients ne peut pas être vide!")
+    .bail()
+    .isLength({ min: 10, max: 50 })
+    .withMessage("Entre 10 et 50 caractères!")
+    .bail(),
+  check("type")
+    .notEmpty()
+    .withMessage("Type ne peut pas être vide!")
+    .bail()
+    .isLength({ min: 4 })
+    .withMessage("Minimum 4 caractères requis!")
+    .bail(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res
+        .status(StatusCodes.UNPROCESSABLE_ENTITY)
+        .json({ errors: errors.array() });
+    next();
+  },
+];
 const deleteRequestValidator = [
   param("id")
     .not()
@@ -54,4 +103,4 @@ const deleteRequestValidator = [
   },
 ];
 
-export { addRequestValidator, deleteRequestValidator };
+export { addRequestValidator, updateRequestValidator, deleteRequestValidator };
--- src/route/Recette.js
+++ src/route/Recette.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 
 import Recette from "../controllers/Recette.js";
-import { addRequestValidator, deleteRequestValidator } from "../Validator.js";
+import {
+  addRequestValidator,
+  deleteRequestValidator,
+  updateRequestValidator,
+} from "../Validator.js";
 const router = Router();
-
 
 router.get("/recettes", Recette.getRecettes);
 
 router.post("/recettes", addRequestValidator, Recette.createRecette);
 
-router.put("/recettes/:id", deleteRequestValidator, Recette.updateRecette);
+router.put("/recettes/:id", updateRequestValidator, Recette.updateRecette);
 
 router.delete("/recettes/:id", deleteRequestValidator, Recette.deleteRecette);
 
